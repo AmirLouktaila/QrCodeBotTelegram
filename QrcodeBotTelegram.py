ca823682de35e897f5dd24e4ca48e669e7466014
AmirLouktaila <86385687+AmirLouktaila@users.noreply.github.com>
2023-05-31 22:30:00 -0700

    Update QrcodeBotTelegram.py

--- QrcodeBotTelegram.py
+++ QrcodeBotTelegram.py
@@ -1,8 +1,9 @@
 import telebot
 import qrcode
 import random
-import requests
-bot = telebot.TeleBot("YourTokenBot")
+import cv2
+from pyzbar.pyzbar import decode
+bot = telebot.TeleBot("5860213339:AAEaz5qVv8YBmbe-TBzV41ceNUAp2NGYoao")
 a=random.randint(1000,10000000)
 
 @bot.message_handler(commands=['start', 'help'])
@@ -20,11 +21,20 @@ def echo_all(m):
     bot.send_photo(m.chat.id,photo)
     photo.close()
 
-
-
-
-
-
+@bot.message_handler(content_types=['photo'])
+def photo(message):
+    print ('message.photo ='), message.photo
+    fileID = message.photo[-1].file_id
+    print ('fileID ='), fileID
+    file_info = bot.get_file(fileID)
+    print ('file.file_path ='), file_info.file_path
+    downloaded_file = bot.download_file(file_info.file_path)
+    names_code="photo"+str(a)+".png"
+    with open(names_code, 'wb') as new_file:
+        new_file.write(downloaded_file)
+    img=cv2.imread(names_code)
+    for code in decode(img):
+        bot.reply_to(message,code.data.decode('utf-8'))
 
 
 
